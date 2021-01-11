93382866a7f70f9be74409fa3af58ca49d292670
bronwyncarr <bronwynacarr@gmail.com>
2021-01-11 13:56:25 +1100

    adds styled components

---
+++ src/Styled.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Container = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+`
+export const Info = styled.div `
+  display: flex;
+  padding: 2rem;
+  justify-content: center;
+  flex-direction: column;
+  background-color: white;
+  min-width: 500px;
+`
+
+export const Heading = styled.h1 `
+  font-family: 'Roboto';
+  color: #fff;
+  font-size: 4rem;
+`
+
+export const WeatherInfo = styled.div `
+  font-family: 'Roboto';
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: space-evenly;
+`
+
+export const WeatherText = styled.div `
+
+`
